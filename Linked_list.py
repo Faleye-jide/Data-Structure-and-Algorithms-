68367cb8513b2922088f78b1ff78aaea7d145739
Jide <52969708+Faleye-jide@users.noreply.github.com>
2021-04-27 01:26:10 +0100

    Update Linked_list.py

--- Linked_list.py
+++ Linked_list.py
@@ -40,73 +40,40 @@ class linked_list:
             print(cur_node.data)
             cur_node = cur_node.next
 #
-#     def insert_values(self, data_list):
-#         self.head = None
-#         for data in data_list:
-#             self.append(data_list)
-#
-
-#
-#
-#     def remove(self, index):
-#         if index < 0:
-#             return
-#         if index >= self.get_length():
-#             return
-#         if index == 0:
-#             self.head = self.head.next
-#             return
-#
-#         count = 0
-#         cur_node = self.head
-#         while cur_node:
-#             if count == index - 1:
-#                 cur_node.next = cur_node.next.next
-#                 break
-#
-#             cur_node = cur_node.next
-#             count += 1
-#
-#
-#
-# llist = linked_list()
-# llist.insert_values(["jide","alex","ayo"])
-# print("length:", llist.get_length())
-# llist.append("A")
-# llist.prepend("B")
-# llist.prepend("C")
-# llist.print_list()
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.append(data_list)
 
 
-# NumberList = []
-#
-# size = int(input("Enter the total elements: "))
-# for i in range(size):
-#     element = int(input("Enter a %d value: "%i))
-#     NumberList.append(element)
-# print("find odd and even")
-# #
-# # for i in range(size):
-# #     if (NumberList[i] % 2 )== 0:
-# #         print (NumberList[i]), 'is even'
-# #     else:
-# #         print (NumberList[1]), 'is odd'
-#
-# # # square = [x**2 for x in range(10)]
-# # # print(square)
-# #
-# # squared = []
-# # for i in range(0, 10):
-# #     squared.append(i**2)
-# # print(squared)
-#
-# # comb = []
-# # for i in [1,2,3]:
-# #     for j in [1,3,4]:
-# #         if i != j:
-# #             comb.append((i, j))
-# # print(comb)
-#
-# comb = [(i, j) for i in [1,2,3,] for j in [1,3,4] if i != j]
-# print(comb)
+
+
+    def remove(self, index):
+        if index < 0:
+            return
+        if index >= self.get_length():
+            return
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        cur_node = self.head
+        while cur_node:
+            if count == index - 1:
+                cur_node.next = cur_node.next.next
+                break
+
+            cur_node = cur_node.next
+            count += 1
+
+
+
+llist = linked_list()
+llist.insert_values(["jide","alex","ayo"])
+print("length:", llist.get_length())
+llist.append("A")
+llist.prepend("B")
+llist.prepend("C")
+llist.print_list()
 
